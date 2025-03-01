45e53d3e811951b43ef0ded2584db2c9edcbb27b
SamiBachiriPXL <sami.Bachiri@student.pxl.be>
2025-03-01 09:41:58 +0100

    startup

---
+++ HackatonAE/src/main/java/be/ae/hackatonae/HackatonAeApplication.java
@@ -0,0 +1,13 @@
+package be.ae.hackatonae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonAeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackatonAeApplication.class, args);
+    }
+
+}
---
+++ HackatonAE/src/test/java/be/ae/hackatonae/HackatonAeApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ae.hackatonae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonAeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
