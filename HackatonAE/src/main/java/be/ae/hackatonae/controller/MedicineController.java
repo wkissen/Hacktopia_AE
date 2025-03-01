18f6ea6f0dcba85fc1a04d4585904b20c0558509
SamiBachiriPXL <sami.Bachiri@student.pxl.be>
2025-03-01 13:32:22 +0100

    medicine

--- HackatonAE/src/main/java/be/ae/hackatonae/controller/MedicineController.java
+++ HackatonAE/src/main/java/be/ae/hackatonae/controller/MedicineController.java
@@ -1,9 +1,15 @@
 package be.ae.hackatonae.controller;
 
+import be.ae.hackatonae.domain.Medicine;
 import be.ae.hackatonae.service.MedicineService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/medicine")
@@ -13,5 +19,11 @@ public class MedicineController {
     @Autowired
     public MedicineController(MedicineService medicineService) {
         this.medicineService = medicineService;
+    }
+
+    @RequestMapping("/{Treatment}")
+    public ResponseEntity<?> getMedicine(@PathVariable String Treatment) {
+        List<Medicine> medicine = medicineService.getMedicine(Treatment);
+        return ResponseEntity.ok(medicine);
     }
 }
